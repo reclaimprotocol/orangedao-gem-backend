9b68ef8032869b3cd2290d02e28297a3996de3fe
Sweta Shaw <swt.shw94@gmail.com>
2023-03-27 09:44:16 +0530

    add callback and html response

--- index.ts
+++ index.ts
@@ -3,15 +3,25 @@ import express, { Express, Request, Response } from 'express';
 import serverless from 'serverless-http';
 import AWS from 'aws-sdk';
 import bodyParser from 'body-parser'
-import {Reclaim, generateUuid} from 'template-client-sdk'
+import { Reclaim } from '@reclaimprotocol/template-client-sdk'
+import { Claim } from './utils/types';
+import { status } from './utils/constants';
+import P from 'pino'
 
 const app: Express = express();
+
+const logger = P()
 
 app.use(cors())
 app.use(bodyParser.json())
 
+app.set('views', './views');
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
 const USERS_TABLE = process.env.USERS_TABLE;
 const callbackUrl = process.env.CALLBACK_URL;
+const redirectBaseUrl = process.env.REDIRECT_BASE_URL;
 
 const dynamoDb = new AWS.DynamoDB;
 
@@ -21,15 +31,14 @@ const connection = reclaim.getConsent(
   [
     {
       provider: 'yc-login',
-      params: { }
+      params: {}
     }
   ]
 )
 
 app.get("/", (req, res, next) => {
-  return res.status(200).json({
-    message: "Hello from root!",
-  });
+  res.status(200).json( {msg: "Orange DAO gem backend"})
+  return
 });
 
 app.get("/user/:userAddress", (req: Request, res: Response, next) => {
@@ -42,7 +51,7 @@ app.get("/user/:userAddress", (req: Request, res: Response, next) => {
   const params: AWS.DynamoDB.GetItemInput = {
     TableName: USERS_TABLE!,
     Key: {
-      userAddress: {S: userAddress},
+      userAddress: { S: userAddress },
     },
   };
 
@@ -53,7 +62,7 @@ app.get("/user/:userAddress", (req: Request, res: Response, next) => {
     }
     if (result.Item) {
       const { userAddress, templateLink, callbackId } = result.Item;
-      res.json({ userAddress, templateLink, callbackId });
+      res.json(result.Item);
       return
     } else {
       res.status(404).json({ error: "User not found" });
@@ -62,14 +71,14 @@ app.get("/user/:userAddress", (req: Request, res: Response, next) => {
   });
 })
 
-app.post("/adduser/", async(req: Request<{}, {}, {userAddress: string}>, res: Response, next) => {
+app.post("/adduser/", async (req: Request<{}, {}, { userAddress: string }>, res: Response, next) => {
   const { userAddress } = req.body;
   if (typeof userAddress !== 'string') {
     res.status(400).json({ error: '"userAddress" must be a string' });
     return
   }
 
-  const callbackId = `${userAddress}-${generateUuid()}`;
+  const callbackId = `${userAddress}`;
 
   const template = (await connection).generateTemplate(callbackId);
   const templateUrl = template.url
@@ -78,24 +87,27 @@ app.post("/adduser/", async(req: Request<{}, {}, {userAddress: string}>, res: Re
     TableName: USERS_TABLE!,
     ConditionExpression: "attribute_not_exists(userAddress)",
     Item: {
-      userAddress: {S: userAddress},
-      templateLink: {S: templateUrl},
-      callbackId: {S: callbackId},
-      claimStatus: {S: "pending"}
+      userAddress: { S: userAddress },
+      templateLink: { S: templateUrl },
+      callbackId: { S: callbackId },
+      claimStatus: { S: status.PENDING },
+      createdAtS: { S: new Date().toISOString() }
     },
   };
 
   dynamoDb.putItem(params, (error) => {
     if (error) {
-      res.status(400).json({ error:  `Could not create user ${error}` });
+      logger.error(`[ERROR] Could not create user ${error}`)
+      res.status(400).json({ error: `Could not create user ${error}` });
       return
     }
+    logger.info(`User ${userAddress} created`)
     res.json({ templateUrl });
     return
   });
 })
 
-app.post("/callback/:id", (req: Request<{}, {}, {userId: string, claim: string}>, res: Response, next) => {
+app.post("/callback/:userAddress", (req: Request, res: Response, next) => {
 
   // TODO: verify the correctness of the proof
 
@@ -111,8 +123,8 @@ app.get('/status/:callbackId', (req: Request, res: Response, next) => {
     ExpressionAttributeValues: {
       ":v_callbackId": {
         S: callbackId
-       }
-     }, 
+      }
+    },
     KeyConditionExpression: "callbackId = :v_callbackId",
     ProjectionExpression: "claimStatus",
   };
